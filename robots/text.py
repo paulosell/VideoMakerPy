e859e56faccdfeb00ee9bc5ee4441959d4454454
Paulo Sell <sellpaulof@gmail.com>
2019-04-07 01:24:39 -0300

    feat: text

---
+++ robots/text.py
@@ -0,0 +1,39 @@
+import Algorithmia
+import spacy
+
+
+
+class Text:
+
+    def fetchContentFromWikipedia(self, searchTerm):
+        algorithmiaAutenticaded = Algorithmia.client('simFwP43whs7NkfjT3T7uw7jbFX1')
+        wikipediaAlgorithm = algorithmiaAutenticaded.algo('web/WikipediaParser/0.1.2')
+        query = {
+                "articleName": searchTerm,
+                "lang": "en"
+                }
+        wikipediaResponse = wikipediaAlgorithm.pipe(query).result
+        return wikipediaResponse
+    
+    def removeBlankLinesAndMarkdown(self,sourceContentOriginal):
+        allLines = str(sourceContentOriginal).split('\n')
+        withoutBlankLinesAndMarkdown = ""
+        for line in allLines:
+            if ((len(line.strip()) > 0) or (line.strip()[0] != '=')):
+                withoutBlankLinesAndMarkdown = withoutBlankLinesAndMarkdown + line
+        return withoutBlankLinesAndMarkdown
+    
+    def breakIntoSentences(self,text):
+        nlp = spacy.load('en')
+        doc = nlp(str(text))
+        sentences = []
+        for sentence in doc.sents:
+            sentences.append(sentence)
+        return sentences
+            
+
+
+
+
+
+
